e9e12041880a0cfcf03d63c1ebf5a7b35a5f863f
Duda Wiltiner <eduardawiltiner@gmail.com>
2022-01-26 20:20:59 -0300

    Adaptando o setup com css e boas praticas de formataçao com eslint

---
+++ src/css/AppCSS.js
@@ -0,0 +1,9 @@
+import tw from "tailwind-styled-components";
+
+export const StyledApp = tw.span`
+  px-
+  text-5xl
+  text-white
+  box-decoration-slice
+  bg-gradient-to-r from-indigo-600 to-pink-500
+`  
