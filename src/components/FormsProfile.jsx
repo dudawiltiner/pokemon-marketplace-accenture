16249253f00176b028af8b8ebafd97812a7547be
Duda Wiltiner <eduardawiltiner@gmail.com>
2022-01-26 09:09:44 -0300

    Setup do projeto

---
+++ src/components/FormsProfile.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function FormsProfile() {
+  return <div></div>;
+}
---
+++ src/components/ModalPokemon.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ModalPokemon() {
+  return <div></div>;
+}
