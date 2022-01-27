cb99a992cc47de5cc9c2fef613914f8393a11f85
Duda Wiltiner <eduardawiltiner@gmail.com>
2022-01-27 16:29:41 -0300

    Estilizaçao da pagina de Login

--- src/components/FormsLogin.jsx
+++ src/components/FormsLogin.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
-import { Login } from '../styles/LoginCSS';
+import * as S from '../styles/LoginCSS';
 
 export default function FormsLogin() {
   return (
-    <Login>
-      <form />
-    </Login>
+    <S.Login>
+      <S.ContentLogin>
+        <S.Logo src="./logo.png" alt="logo" />
+        <S.Title>Faça o login</S.Title>
+        <S.FormsLogin>
+          <S.Label htmlFor="email">
+            <S.Subtitle>E-mail</S.Subtitle>
+            <S.Input
+              id="email"
+              name="email"
+              type="email"
+              placeHolder="Digite o seu e-mail"
+            />
+          </S.Label>
+          <S.Label htmlFor="password">
+            <S.Subtitle>Senha</S.Subtitle>
+            <S.Input
+              id="password"
+              name="password"
+              type="password"
+              placeHolder="Digite a sua senha"
+            />
+          </S.Label>
+          <S.ChangePassword type="button">
+            <p className="text-center pl-14 w-96">Esqueceu a sua senha?</p>
+          </S.ChangePassword>
+          <S.ButtonIn type="button">GO!</S.ButtonIn>
+          <S.Register>
+            <p className="text-lg text-gray-500">Ainda não é um treinador de Pokémon?</p>
+            <S.ButtonRe>Cadastre-se agora</S.ButtonRe>
+          </S.Register>
+        </S.FormsLogin>
+      </S.ContentLogin>
+    </S.Login>
   );
 }
