dca322ec1b39b593e4f31e0bb0d470aeee5fd83e
Duda Wiltiner <eduardawiltiner@gmail.com>
2022-01-31 18:37:28 -0300

    Juntando com a parte do Wanderson

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,5 +1,38 @@
 import React from 'react';
+import NavBar from '../components/NavBar';
+import * as S from '../styles/ShoppingCartCSS';
+// import useVerifyAuth from '../hooks/useVerifyAuth';
 
 export default function ShoppingCart() {
-  return <div />;
+  // useVerifyAuth();
+
+  return (
+    <S.Container>
+      <NavBar />
+      <S.CartMain>
+        <S.CartInfo>
+          <S.Produto>Produto</S.Produto>
+          <S.Quantidade>Quantidade</S.Quantidade>
+          <S.Preço>Preço</S.Preço>
+          <S.Subtotal>Subtotal</S.Subtotal>
+        </S.CartInfo>
+        <S.productInfo>
+          <S.imageProduct src="./pikachu.png" alt="" />
+          <S.PokemonName>Pikachu</S.PokemonName>
+          <S.CartControl>
+            <S.SubProduct type="submit">-</S.SubProduct>
+            <S.ProductAmount>1</S.ProductAmount>
+            <S.AddProduct type="submit">+</S.AddProduct>
+          </S.CartControl>
+          <S.ProductPrice>R$ 20.000,00</S.ProductPrice>
+          <S.ProductSubtortal>R$ 20.000,00</S.ProductSubtortal>
+          <S.RemoveProduct type="submit">
+            <img src="./trash.png" alt="" />
+          </S.RemoveProduct>
+          <S.HorizontalRow />
+        </S.productInfo>
+      </S.CartMain>
+    </S.Container>
+
+  );
 }
