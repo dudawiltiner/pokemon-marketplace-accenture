e5939f6339ee13924533e243de1464870fd58c80
Duda Wiltiner <eduardawiltiner@gmail.com>
2022-02-01 17:20:07 -0300

    Criada a pagina 404 para usabilidade do usuario

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,5 +4,6 @@ import Profile from './Profile';
 import ShoppingCart from './ShoppingCart';
 import ChangePassword from './ChangePassword';
 import Register from './Register';
+import Custom404 from './Custom404';
 
-export { Home, Login, Profile, ShoppingCart, ChangePassword, Register };
+export { Home, Login, Profile, ShoppingCart, Custom404, ChangePassword, Register };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin 20s linear infinite',
+      }
+    },
   },
   plugins: [],
 };
