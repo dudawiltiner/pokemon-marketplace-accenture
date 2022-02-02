f0f41d0d168571d09e477cc4723ec4cb92c26cac
Wanderson Vicente <wanderson.vs@outlook.com>
2022-02-02 17:23:40 -0300

    Implementaçao da logica de soma do componente CartTotal

--- src/components/CartCounter.jsx
+++ src/components/CartCounter.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable react/prop-types */
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
+// import { ShoppingCartContext } from '../context/ShoppingCartContext';
 import { plusPokemonAction, subPokemonAction } from '../redux/actions';
 import * as S from '../styles/ShoppingCartCSS';
 
-export default function CartCounter({ pokemonBought, total, valueTotal }) {
-  // const state = useSelector((state) => state.count);
+export default function CartCounter({ pokemonBought }) {
+  // const context = useContext(ShoppingCartContext);
+
+  // const { total, setTotal } = context;
 
   const dispatch = useDispatch();
 
@@ -19,11 +22,13 @@ export default function CartCounter({ pokemonBought, total, valueTotal }) {
 
   const [newPrice, setNewPrice] = useState(pokemonBought.price);
 
-  const handlePrice = () => {
+  const handlePrice = async () => {
     const mult = 1000;
     const initialPrice = parseFloat(pokemonBought.price) * mult;
     const subPrice = initialPrice * pokemonBought.count;
-    total(valueTotal + subPrice);
+    // const totalAmount = total + subPrice;
+    // console.log(totalAmount);
+    // await setTotal(totalAmount);
     setNewPrice(subPrice.toLocaleString('en-US').replace(',', '.'));
     // eslint-disable-next-line max-len
     // console.log((parseFloat(pokemonBought.price) * mult).toLocaleString('en-US').replace(',', '.'));
@@ -31,7 +36,8 @@ export default function CartCounter({ pokemonBought, total, valueTotal }) {
 
   useEffect(() => {
     handlePrice();
-  });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <>
       <S.CartCounter>
