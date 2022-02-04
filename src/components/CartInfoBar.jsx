4a673320d414c7523a588726f804b0b0167ee809
Duda Wiltiner <eduardawiltiner@gmail.com>
2022-02-03 22:23:13 -0300

    modicficaçao do heroku

--- src/components/CartInfoBar.jsx
+++ src/components/CartInfoBar.jsx
@@ -4,12 +4,10 @@ import * as S from '../styles/ShoppingCartCSS';
 export default function CartInfoBar() {
   return (
     <S.CartInfo>
-
       <S.Produto>Product</S.Produto>
       <S.Quantidade>Amount</S.Quantidade>
       <S.Preço>Price</S.Preço>
       <S.Subtotal>Subtotal</S.Subtotal>
-
     </S.CartInfo>
   );
 }
